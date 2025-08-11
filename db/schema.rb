233b0120ced0cc35d75ccde54f98695a5f94cb7d
古賀彩子 <kogaayako@kogaayakonoMacBook-Air.local>
2025-08-11 23:54:16 +0900

    投稿一覧ページを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_15_161227) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_11_133115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "name", null: false
@@ -40,4 +49,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_15_161227) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "boards", "users"
 end
