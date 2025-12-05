351cb459437b20f3d123959bec39cbaaab5b3ffd
古賀彩子 <kogaayako@kogaayakonoMacBook-Air.local>
2025-12-05 23:22:25 +0900

    店名検索実装

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,6 +3,14 @@ class Shop < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "cashless", "created_at", "density", "id", "latitude", "longitude", "name", "opening_hours", "phone", "photo_url", "place_id", "postal_code", "rating", "updated_at", "website"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["favorites", "reviews", "user"]
+  end
+
   def average_rating
     reviews.average(:rating)&.round(1) || 0
   end
