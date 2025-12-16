8e10e82f728fbb2e141d9542734d1ed09195fa06
古賀彩子 <kogaayako@kogaayakonoMacBook-Air.local>
2025-12-16 17:43:38 +0900

    プライバシーポリシー作成

--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  get 'privacy', to: 'static_pages#privacy'
+  get 'terms', to: 'static_pages#terms'
 end
